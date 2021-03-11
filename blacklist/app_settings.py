6ef5760b7fac10654c52fe07019645ca1b2c39a4
AaronKable <aaronkable@gmail.com>
2021-03-11 14:00:26 +0800

    rename-able blacklist

--- blacklist/app_settings.py
+++ blacklist/app_settings.py
@@ -1,3 +1,3 @@
 from django.conf import settings
 
-BLACKLIST_STATE_NAME = getattr(settings, 'BLACKLIST_STATE_NAME', "BlackList")
+BLACKLIST_STATE_NAME = getattr(settings, 'BLACKLIST_STATE_NAME', "Blacklist")
