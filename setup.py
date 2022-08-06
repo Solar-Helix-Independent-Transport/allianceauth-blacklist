cb52e699f712727d61b15f53e746dd6d00d55da6
Aaron Kable <aaronkable@gmail.com>
2022-08-06 19:15:48 +0800

    fix tests

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 install_requires = [
-    'allianceauth>=2.8.0',
+    'allianceauth',
 ]
 
 setup(
