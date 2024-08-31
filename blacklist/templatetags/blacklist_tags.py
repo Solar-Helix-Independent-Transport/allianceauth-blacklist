4fd874ed68c27b905706110ba768b8a587f9e39a
Ariel Rin <joel.falknau@gmail.com>
2024-08-31 19:06:27 +1000

    Modernize (#12)
    * translate and template fixes
    
    * update pre-commit
    
    * modernize build and project info
    
    * aa4 tests
    
    * pre-commit spam
    
    * modern django init file
    
    * update precommit
    
    * pyproject checking
    
    * update pre-commit
    
    * update github actions
    
    * update test celery config from upstream
    
    * formatting
    
    * update makefile for flit

--- blacklist/templatetags/blacklist_tags.py
+++ blacklist/templatetags/blacklist_tags.py
@@ -5,13 +5,13 @@ from django.template.defaulttags import register
 @register.simple_tag()
 def eve_logo(id, name, cat, size):
     if cat == "character":
-        return mark_safe("<img class=\"img-circle\" src=\"https://images.evetech.net/characters/%s/portrait?size=%s\" style=\"height: %spx; width: %spx;\" title=\"%s\">" % (id, size, size, size, name))
+        return mark_safe("<img class=\"img-circle\" src=\"https://images.evetech.net/characters/{}/portrait?size={}\" style=\"height: {}px; width: {}px;\" title=\"{}\">".format(id, size, size, size, name))
 
     elif cat == "corporation":
-        return mark_safe("<img class=\"img-circle\" src=\"https://images.evetech.net/corporations/%s/logo?size=%s\" style=\"height: %spx; width: %spx;\" title=\"%s\">" % (id, size, size, size, name))
+        return mark_safe("<img class=\"img-circle\" src=\"https://images.evetech.net/corporations/{}/logo?size={}\" style=\"height: {}px; width: {}px;\" title=\"{}\">".format(id, size, size, size, name))
 
     elif cat == "alliance":
-        return mark_safe("<img class=\"img-circle\" src=\"https://images.evetech.net/alliances/%s/logo?size=%s\" style=\"height: %spx; width: %spx;\" title=\"%s\">" % (id, size, size, size, name))
+        return mark_safe("<img class=\"img-circle\" src=\"https://images.evetech.net/alliances/{}/logo?size={}\" style=\"height: {}px; width: {}px;\" title=\"{}\">".format(id, size, size, size, name))
 
     else:
         return ""
--- blacklist/urls.py
+++ blacklist/urls.py
@@ -1,17 +1,18 @@
+from django.urls import path
 from django.urls import re_path
 from . import views
 
 app_name = 'blacklist'
 
 urlpatterns = [
-    re_path(r'^set/$', views.blacklist_set_search_character, name='set'),
-    re_path(r'^notes/$', views.note_board, name='note_board'),
-    re_path(r'^blacklist/$', views.blacklist, name='blacklist'),
+    path('set/', views.blacklist_set_search_character, name='set'),
+    path('notes/', views.note_board, name='note_board'),
+    path('blacklist/', views.blacklist, name='blacklist'),
     re_path(r'^get_add_note/(?P<eve_id>(\d)*)/$', views.get_add_evenote, name='modal_add'),
     re_path(r'^get_comments/(?P<evenote_id>(\d)*)/$', views.get_evenote_comments, name='modal_comment'),
     re_path(r'^get_edit_note/(?P<evenote_id>(\d)*)/$', views.get_edit_evenote, name='modal_edit'),
     re_path(r'^get_add_comment/(?P<evenote_id>(\d)*)/$', views.get_add_comment, name='modal_add_comment'),
-    re_path(r'^search_names/$', views.search_names, name='search_names'),
+    path('search_names/', views.search_names, name='search_names'),
     re_path(r'^add_comment/(?P<note_id>(\d)*)/$', views.add_comment, name='add_comment'),
     re_path(r'^add_note/(?P<eve_id>(\d)*)/$', views.add_note, name='add_note'),
     re_path(r'^edit_note/(?P<note_id>(\d)*)/$', views.edit_note, name='edit_note'),
