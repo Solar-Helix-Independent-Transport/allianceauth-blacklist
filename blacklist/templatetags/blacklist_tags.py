c40d2c2a778d1b01295f8cb606fd23053a85419b
Aaron Kable <aaronkable@gmail.com>
2026-05-07 20:34:55 +0800

    images

--- blacklist/templatetags/blacklist_tags.py
+++ blacklist/templatetags/blacklist_tags.py
@@ -6,17 +6,17 @@ from django.utils.safestring import mark_safe
 def eve_logo(id, name, cat, size):
     if cat == "character":
         return mark_safe(
-            f'<img class="img-circle" src="https://images.evetech.net/characters/{id}/portrait?size={size}" style="height: {size}px; width: {size}px !important;" title="{name}">'
+            f'<img class="img-circle" src="https://images.evetech.net/characters/{id}/portrait?size={size}" style="min-height: {size}px; min-width: {size}px;max-height: {size}px; max-width: {size}px;" title="{name}">'
         )
 
     elif cat == "corporation":
         return mark_safe(
-            f'<img class="img-circle" src="https://images.evetech.net/corporations/{id}/logo?size={size}" style="height: {size}px; width: {size}px !important;" title="{name}">'
+            f'<img class="img-circle" src="https://images.evetech.net/corporations/{id}/logo?size={size}" style="min-height: {size}px; min-width: {size}px;max-height: {size}px; max-width: {size}px;" title="{name}">'
         )
 
     elif cat == "alliance":
         return mark_safe(
-            f'<img class="img-circle" src="https://images.evetech.net/alliances/{id}/logo?size={size}" style="height: {size}px; width: {size}px !important;" title="{name}">'
+            f'<img class="img-circle" src="https://images.evetech.net/alliances/{id}/logo?size={size}" style="min-height: {size}px; min-width: {size}px;max-height: {size}px; max-width: {size}px;" title="{name}">'
         )
 
     else:
