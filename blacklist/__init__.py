f87d1cb6e96622818e930f465be790562285495f
Aaron Kable <aaronkable@gmail.com>
2026-05-30 17:51:00 +0800

    vbump

--- blacklist/__init__.py
+++ blacklist/__init__.py
@@ -1,6 +1,6 @@
 """
 Integration with Alliance Auth's State System, creates an maintains a Blacklisted State to ensure no services access is granted to Blacklisted users
 """
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __title__ = "Blacklist"
 __url__ = "https://github.com/Solar-Helix-Independent-Transport/allianceauth-blacklist"
